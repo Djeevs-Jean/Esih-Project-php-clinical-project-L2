1779146e92353dad5a618777fb2aee5184bc81d2
djeevs <jeanyvesbadette@gmail.com>
2021-08-03 12:27:19 +0200

    to add src

---
+++ src/lists_medecin.php
@@ -0,0 +1,40 @@
+<?php
+require_once("../main.php");
+include("../elements/header.php"); 
+
+
+// recuperation liste des medecins
+$medecins = lists_medecins("rott");
+
+$ids = Idmedecin("rott");
+
+?>
+
+<!-- Listes des Medecins -->
+
+<!-- Call to Action-->
+    <div class="container">
+        <div class="card text-white bg-secondary my-5 py-4 text-center">
+            <div class="card-body"><p class="text-white m-0">Les medecins de Hospital Djeevs avec leurs spécialités!</p></div>
+        </div>
+
+        <div class="row">
+        
+            <?php foreach($medecins as $value): ?>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <img class="card-img-top" src="img/photo_articles/ile-a-vache/ile-a-vache-2.jpg" alt="medecin " />
+                        <div class="card-body">
+                            <h3 class="card-title display-5 fw-italic">DR. <?= $value["nom"]?> <?= $value["prenom"] ?></h3>
+                            <h6 class="card-text">Le medecin a comme specialites de <?= $value['specialisation'] ?>.</h6>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-md btn-block" href="src/req/medecinpost/">Voir plus</a></div>
+                    </div>
+                </div>
+            <?php endforeach?>
+
+        </div>
+    </div>
+
+
+<?php include("../elements/footer.php"); ?>
---
+++ src/prescription.php
@@ -0,0 +1,87 @@
+<?php
+require_once("../main.php");
+include("../elements/header.php"); 
+
+$Idconsultation = Idconsultation("rott");
+
+
+$errors = array();
+$success = array();
+if(($_POST)){
+    // verifie si les champ ne sont pas vides
+    if(empty($_POST['ordonnance']))
+    {
+        $errors["errors"] = "votre ordonnance n'est pas valide";
+    } 
+    
+    if (empty($errors)) {
+        create_prescription("rott");
+
+        // succes on on enregistre data
+        $success["success"] = "vous a avez enregistrez";
+    }
+
+}
+
+?>
+
+<section class="py-5">
+    <div class="container px-5 my-5">
+        <div class="row gx-5 justify-content-center">
+            <div class="col-lg-8 col-xl-6">
+                <div class="text-center">
+                    <h2 class="fw-bolder">Create a Prescription</h2>
+                    <p class="lead fw-normal text-muted mb-5">Liste des consultations faits par un médecin.</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Error Champ   -->
+        <?php if($errors): ?>
+            <div class="alert alert-danger">
+                <ul>
+                    <?php foreach($errors as $error): ?>
+                        <li><?= $error ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endif ?>
+
+        <!-- Succes Champ   -->
+        <?php if($success): ?>
+            <div class="alert alert-success text-center">
+                <!-- <ul> -->
+                    <?php foreach($success as $success): ?>
+                        <?= $success ?>
+                    <?php endforeach; ?>
+                <!-- </ul> -->
+            </div>
+        <?php endif ?>
+
+        <div class="row gx-5 justify-content-center">
+            <form action="" method="post" class="form-group">
+
+                    <div class="form-group col-md-12 mb-4">
+                        <label for="Idconsultation ">Idconsultation </label>
+                        <select name="Idconsultation" id="name" class="form-control">
+                            <?php foreach($Idconsultation as $k): ?>
+                                <?php foreach($k as $v): ?>
+                                    <option value="<?=$v ?>"><?= $v ?></option>
+                                <?php endforeach; ?>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+        
+                    <div class="form-group col-md-12 mb-4">
+                        <input type="text" name="ordonnance" class="form-control" placeholder="ordonnance">
+                    </div>                            
+
+                <button type="submit" class="btn btn-primary form-control mt-4">Create Prescription</button>
+
+            </form>
+        </div>
+        
+    </div>
+</section>
+
+<?php include("../elements/footer.php"); ?>
