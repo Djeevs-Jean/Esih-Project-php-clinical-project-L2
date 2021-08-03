bea673ec3433e683629a744eeefe46ec0a40d852
djeevs <jeanyvesbadette@gmail.com>
2021-08-03 12:52:02 +0200

    img to src

--- src/lists_medecin.php
+++ src/lists_medecin.php
@@ -23,7 +23,7 @@ $ids = Idmedecin("rott");
             <?php foreach($medecins as $value): ?>
                 <div class="col-md-4 mb-5">
                     <div class="card h-100">
-                        <img class="card-img-top" src="img/photo_articles/ile-a-vache/ile-a-vache-2.jpg" alt="medecin " />
+                    <img class="card-img-top" src="../dist/img/img-doc.jpg" alt="medecin " />
                         <div class="card-body">
                             <h3 class="card-title display-5 fw-italic">DR. <?= $value["nom"]?> <?= $value["prenom"] ?></h3>
                             <h6 class="card-text">Le medecin a comme specialites de <?= $value['specialisation'] ?>.</h6>
