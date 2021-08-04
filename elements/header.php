703be3ca4548051c98d41ceecdbf831c2f26b789
djeevs <jeanyvesbadette@gmail.com>
2021-08-04 03:59:17 +0200

    to add elements

--- elements/header.php
+++ elements/header.php
@@ -18,7 +18,7 @@
         <!--  navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container px-5">
-                <a class="navbar-brand" href="../src/HomePage.php">Hospital Djeevs</a>
+                <a class="navbar-brand" href="../src/homepage.php">Hospital Djeevs</a>
                 <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button> -->
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto  mb-lg-0">
@@ -28,7 +28,7 @@
                             <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Patient</a>
                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
                                 <li><a class="dropdown-item" href="../src/patient.php">Create Patient</a></li>
-                                <li><a class="dropdown-item" href="../src/lists_patients.php">List Patient</a></li>
+                                <li><a class="dropdown-item" href="../src/liste_patient.php">Liste Patient</a></li>
                             </ul>
                         </li>
 
@@ -36,7 +36,7 @@
                             <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Medecin</a>
                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
                                 <li><a class="dropdown-item" href="../src/medecin.php">Create Medecin</a></li>
-                                <li><a class="dropdown-item" href="../src/lists_medecin.php">List Medecin</a></li>
+                                <li><a class="dropdown-item" href="../src/liste_medecin.php">Liste Medecin</a></li>
 
                             </ul>
                         </li>
@@ -45,7 +45,7 @@
                             <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Consultation</a>
                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
                                 <li><a class="dropdown-item" href="../src/consultation.php">Create Consultation</a></li>
-                                <li><a class="dropdown-item" href="../src/lists_consultations.php">List Consultation</a></li>
+                                <li><a class="dropdown-item" href="../src/liste_consultation.php">Liste Consultation</a></li>
                             </ul>
                         </li>
 
@@ -56,7 +56,7 @@
                             </ul>
                         </li>
 
-                        <li class="nav-item"><a class="nav-link" href="#!">Services</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../src/liste_consultaion_prescription.php">Services</a></li>
                         
                     </ul>
                 </div>
