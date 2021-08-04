e28b277c1eb7a2c3074a9f6fb9d75f303e17dab3
djeevs <jeanyvesbadette@gmail.com>
2021-08-04 02:52:06 +0200

    start

--- elements/header.php
+++ elements/header.php
@@ -11,29 +11,48 @@
         <!-- Bootstrap css-->
         
         <link href="/dist/css/bootstrap.min.css" rel="stylesheet" />
+                <!-- style css -->
+        <link href="/dist/style.css" rel="stylesheet" />
     </head>
     <body>
-        <!-- Responsive navbar-->
+        <!--  navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container px-5">
-                <a class="navbar-brand" href="../src/lists_patients.php">Hospital Djeevs</a>
+                <a class="navbar-brand" href="../src/HomePage.php">Hospital Djeevs</a>
                 <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button> -->
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="../src/lists_patients.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                    <ul class="navbar-nav ms-auto  mb-lg-0">
+
 
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Pages</a>
+                            <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Patient</a>
                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
-                                <li><a class="dropdown-item" href="blog-home.html">Blog Home</a></li>
-                                <li><a class="dropdown-item" href="/src/medecin.php">Medecin</a></li>
-                                <li><a class="dropdown-item" href="/src/consultation.php">Consultation</a></li>
-                                <li><a class="dropdown-item" href="/src/prescription.php">Prescription</a></li>
-                                <li><a class="dropdown-item" href="/src/patient.php">Patient</a></li>
-                                <li><a class="dropdown-item" href="/src/lists_patients.php">List Patient</a></li>
-                                <li><a class="dropdown-item" href="/src/lists_consultations.php">List Consultation</a></li>
+                                <li><a class="dropdown-item" href="../src/patient.php">Create Patient</a></li>
+                                <li><a class="dropdown-item" href="../src/lists_patients.php">List Patient</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Medecin</a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
+                                <li><a class="dropdown-item" href="../src/medecin.php">Create Medecin</a></li>
+                                <li><a class="dropdown-item" href="../src/lists_medecin.php">List Medecin</a></li>
+
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Consultation</a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
+                                <li><a class="dropdown-item" href="../src/consultation.php">Create Consultation</a></li>
+                                <li><a class="dropdown-item" href="../src/lists_consultations.php">List Consultation</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Prescription</a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
+                                <li><a class="dropdown-item" href="../src/prescription.php">Create Prescription</a></li>
                             </ul>
                         </li>
 
